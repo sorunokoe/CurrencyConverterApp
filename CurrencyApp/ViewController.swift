f8cc78cacf953d5b8641db9666607701ebe27375
Андерсон <Salgara@kazposts-imac.local>
2018-05-10 14:19:07 +0600

    Initial Commit

---
+++ CurrencyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by Yeskendir on 10/05/2018.
+//  Copyright © 2018 Yeskendir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
